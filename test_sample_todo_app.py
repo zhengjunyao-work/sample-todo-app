a017f335781385b5e1bc7f529c07032a3f812822
tech-with-moss <83650740+tech-with-moss@users.noreply.github.com>
2021-06-26 17:52:46 -0400

    updating back to build name env var for capability

--- test_sample_todo_app.py
+++ test_sample_todo_app.py
@@ -4,7 +4,7 @@ import os
 
 url = os.getenv("LT_HUB_URL")
 capabilities = {
-    "build" : "TEST",
+    "build" : os.getenv("LT_BUILD_NAME"),
     "name" : "Quick Test",
     "platform" : "Windows 10",
     "browserName" : "Chrome",
