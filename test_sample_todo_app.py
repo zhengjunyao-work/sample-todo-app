86ac6b585e35606d62dc425e160df8e08493b968
tech-with-moss <83650740+tech-with-moss@users.noreply.github.com>
2021-06-26 21:29:33 -0400

    removing syntax error

--- test_sample_todo_app.py
+++ test_sample_todo_app.py
@@ -23,6 +23,6 @@ driver.find_element_by_name("li3").click()
 textbox = driver.find_element_by_id("sampletodotext")
 textbox.send_keys("Testing")
 driver.find_element_by_id("addbutton").click()
-assert "No results found." not in driver.page_source:
+assert "No results found." not in driver.page_source
 driver.execute_script("lambda-status=passed")
 driver.quit()
